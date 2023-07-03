45bbbf1627ddae0b24dd350ae5d46d649293ffb4
pooominyoung <77039272+pooominyoung@users.noreply.github.com>
2023-07-03 13:47:56 +0900

    Update ProductController.java

--- src/Controller/ProductController.java
+++ src/Controller/ProductController.java
@@ -1,88 +1,110 @@
 package Controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import Domain.Common.Dto.ProdDto;
 import Domain.Common.Service.ProductService;
-
-//
-//ProdService
-//
-//- memberService : MemberService
-//
-//- dao : prodDao
-//
-//- instance() : ProdService
-//
-//+ reqProd(String sid, int product_code) : boolean
-//
-//+ addProd(String sid, int product_code, String product_name)
-//
-//: boolean
-//
-//+updateProd(String sid, int prodcuct_code) : boolean
-//
-//+romoveProd(String sid, int product_code) : boolean
-//
-//+ getInstance() : ProdService
-
-
 
 
 
 public class ProductController {
+	
 	private ProductService service;
+	
 	public ProductController() {
 		service = ProductService.getInstance();
 	}
-	
-	
 	public Map<String,Object> execute (int serviceNo, Map<String,Object>param){
+		
 		if(serviceNo==1) {
-			String sid = (String)param.get("sid");
-			Integer product_code = (Integer)param.get("product_code");
+			List<ProdDto> list = null;
+			try {
+				list = service.reqAllProd();
+			} catch (Exception e) {
 			
-			if(sid==null||product_code==null) {
-				System.out.println("[ERROR] Data Validation Check Error");
-				return null;
+				e.printStackTrace();
 			}
-			
-			Boolean rValue = false;
-			rValue =service.reqProd(product_code);
+			System.out.println("Prod_Select All Block!");
 			Map<String,Object>result = new HashMap();
-			result.put("result", rValue);
+			result.put("result", list);
 			return result;
 			
 		}else if(serviceNo==2) {
 			String sid = (String)param.get("sid");
 			Integer product_code = (Integer)param.get("product_code");
-			String product_name = (String)param.get("product_name");
-//			Integer amount = (Integer)param.get("amount");
-			if(sid==null||product_code==null||product_name == null) {
-				System.out.println("[ERROR] Data Validation Check Error");
-				return null;
-			}
-			Boolean rValue = false;
-			rValue = service.addProd(sid,product_code,product_name);
-			System.out.println("Product_Add Block!");
-			Map<String,Object> result = new HashMap();
-			result.put("result", rValue);
-			return result;
-		}else if(serviceNo==3) {
-			String sid = (String)param.get("sid";)
-			Integer product_code = (Integer)param.get("product_code");
+			
 			if(sid==null||product_code==null) {
 				System.out.println("[ERROR] Data Validation Check Error");
 				return null;
 			}
+			
+			ProdDto dto = null;
+			try {
+				dto =service.reqProd(product_code);
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+			Map<String,Object>result = new HashMap();
+			result.put("result", dto);
+			return result;
+			
+		}else if(serviceNo==3) {
+			String sid = (String)param.get("sid");
+			Integer product_code = (Integer)param.get("product_code");
+			String product_name = (String)param.get("product_name");
+			Integer amount = (Integer)param.get("amount");
+			
+			if(product_code==null||product_name == null || amount ==null) {
+				System.out.println("[ERROR] Data Validation Check Error");
+				return null;
+			}
+			
+			ProdDto dto = new ProdDto(product_code,product_name,amount);
+			
+			Boolean rValue = false;
+			try {
+				rValue = service.addProd(sid,dto);
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+			
+			System.out.println("Product_Add Block!");
+			Map<String,Object> result = new HashMap();
+			result.put("result", rValue);
+			return result;
+		
+		}else if(serviceNo==4) {
+			String sid = (String)param.get("sid");
+			Integer product_code = (Integer)param.get("product_code");
+			String product_name = (String)param.get("product_name");
+			Integer amount = (Integer)param.get("amount");
+			
+			
+			if(product_code==null||product_name==null||amount==null) {
+				System.out.println("[ERROR] Data Validation Check Error");
+				return null;
+			}
+			
+			ProdDto dto = new ProdDto(product_code,product_name,amount);
+			
 			Boolean rValue=false;
-			rValue=service.updateProd(sid,product_code);
+			try {
+				rValue=service.updateProd(sid,dto);
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+			
 			System.out.println("Product_Add Block!");
 			Map<String,Object>result= new HashMap();
 			result.put("result", rValue);
 			return result;
 			
-		}else if(serviceNo==4) {
+		}else if(serviceNo==5) {
 			String sid= (String)param.get("sid");
 			Integer product_code = (Integer)param.get("product_code");
 			if(sid==null||product_code==null) {
@@ -90,12 +112,18 @@ public class ProductController {
 				return null;
 			}
 			Boolean rValue = false;
-			rValue=service.removeProd(sid,product_code);
+			try {
+				rValue=service.removeProd(sid,product_code);
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
 			Map<String,Object> result = new HashMap();
 			result.put("result", rValue);
 			return result;
+		}
+		return null;
+
 	}
 
-	return null;
-}
 }
