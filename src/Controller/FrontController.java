f39800896add6a71a61a47b06ba0139314eb76e3
jaemin <leejm_1994@naver.com>
2023-07-03 09:48:23 +0900

    Session_FrontController_added

--- src/Controller/FrontController.java
+++ src/Controller/FrontController.java
@@ -1,5 +1,60 @@
 package Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FrontController {
 
+	
+	private Map<String,Object> map = new HashMap();
+	
+	
+	// 초기값 설정
+	private void init()
+	{
+		map.put("/order",new OrderController());
+		map.put("/member",new MemberController());
+		map.put("/product",new ProductController());
+	}
+	
+	FrontController()
+	{
+		init();
+	}
+	
+	// 각 컨트롤러에 맞게 다운캐스팅 해주면서 어떠한 서비스를 사용할건지 지정
+	public Map<String,Object> execute(String request,int serviceNo,Map<String,Object> param)
+	{
+		Object controller = map.get(request);
+		Map<String,Object> result = new HashMap();
+		
+		if(controller instanceof MemberController)
+		{
+			MemberController down = (MemberController) controller;
+			result = down.execute(serviceNo,param);
+			System.out.println("MemberController !");
+		}
+		else if(controller instanceof OrderController)
+		{
+			OrderController down = (OrderController) controller;
+			result = down.execute(serviceNo,param);
+			System.out.println("OrderController !");
+		}
+		else if(controller instanceof ProductController)
+		{
+			ProductController down = (ProductController) controller;
+			result = down.execute(serviceNo,param);
+			System.out.println("ProductController !");
+		}
+		else
+		{
+			System.out.println("System ERROR please try again");
+		}
+		
+		return result;
+		
+	}
+	
+	
+	
 }
