e07a55ed41939ff953ef22635bba5e7d8dab6c70
unknown <pak0713@naver.com>
2023-07-05 11:36:25 +0900

    dfd

--- src/Domain/Common/Service/OrderService.java
+++ src/Domain/Common/Service/OrderService.java
@@ -1,6 +1,5 @@
 package Domain.Common.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 import Domain.Common.Dao.MemberDao;
@@ -79,133 +78,174 @@ public class OrderService {
 //			}
 		
 	//==================================피드백
-//		public boolean reqOrder(String id, int odr_amount) throws Exception {
-//			
+		public boolean reqOrder(String sid,String id, int product_code, int odr_amount) throws Exception {
+			
+//			Map<String, Object> login_sid = memberService.login("member1","1234");;
+			// 관리자 로그인 확인, Role 받기
+			String role = memberService.getRole(sid);
+			if (!role.equals("Role_Member")) {
+				System.out.println("[WARN] 관리자만 로그인 할 수 있습니다.");
+				return false;
+			}
+			// 회원 존재 유무 확인
+			MemberDto mdto = memberService.memberSearchOne("Role_user", id);
+
+			if (mdto != null) {
+				// 상품 존재 유무 확인
+				ProdDto pdto = productService.reqProd(product_code);
+				if (pdto != null) {
+					//pDao.UpdateAmount();
+					// 주문완료		
+					oDao.insert(new OrderDto("16",mdto.getId(),pdto.getProduct_code(),"청바지",mdto.getAdr_addr(),odr_amount,null,100));		
+					System.out.println("[INFO] 주문완료");
+					return false;
+				}
+				System.out.println("[INFO] 제품없음.");
+				return false;
+			}
+			System.out.println("[INFO] 해당 회원이 존재하지 않습니다.");
+			return false;
+		}
+		
+		//==================================피드백
+		
+//		public boolean reqOrder2(String sid, String id,int product_code, int odr_amount) throws Exception {
 //			MemberDto mdto = new MemberDto();
 //			ProdDto pdto = new ProdDto();
 //			OrderDto odto = new OrderDto();
 //			
-//			// 관리자 로그인 확인, Role 받기
-//			String role = memberService.getRole(id);
+//			String role = memberService.getRole(sid);
 //			if (!role.equals("Role_Member")) {
 //				System.out.println("[WARN] 관리자만 로그인 할 수 있습니다.");
 //				return false;
 //			}
-//			// 회원 존재 유무 확인
-//			mdto = memberService.memberSearchOne(role, mdto.getId());
-//
-//			if (mdto != null) {
-//				// 상품 존재 유무 확인
-//				pdto = productService.reqProd(pdto.getProduct_code());
-//				if (pdto != null) {
-//					
-//					//pDao.UpdateAmount();
-//					
-//					// 주문완료		
-//					oDao.insert(odto);
-//						
-//					System.out.println("[INFO] 주문완료");
-//					return false;
-//				}
-//				System.out.println("[INFO] 제품없음.");
-//				return false;
-//			}
-//			System.out.println("[INFO] 해당 회원이 존재하지 않습니다.");
-//			return false;
-//		}
-//		
-//	public boolean reqOrder(String id, int odr_amount) throws Exception {
-//		MemberDto mdto = new MemberDto();
-//		ProdDto pdto = new ProdDto();
-//		OrderDto odto = new OrderDto();
-//		
-//		String role = memberService.getRole(id);
-//		if (!role.equals("Role_Member")) {
-//		      System.out.println("[WARN] 관리자만 로그인 할 수 있습니다.");
-//		      return false;
-//		}
-//		
-//	    mdto = memberService.memberSearchOne(role, id);
-//	    if (mdto != null) {
-//	       pdto = productService.reqProd(odto.getProduct_code());
-//	        if (pdto != null) {
-//	            int currentStock = pdto.getAmount();
-//	            if (currentStock >= odr_amount) {
-//	                int updatedStock = currentStock - odr_amount;
-//	                pdto.setAmount(updatedStock);
-//	                productService.updateProd(id, pdto);
-//	                oDao.insert(odto);
-//	                System.out.println("[INFO] 주문완료");
-//	                return true;
-//	            } else {
-//	                System.out.println("[INFO] 주문 수량이 재고보다 많습니다.");
-//	                return false;
-//	            }
-//	        } else {
-//	            System.out.println("[INFO] 해당 상품이 존재하지 않습니다.");
-//	            return false;
-//	        }
-//	    } else {
-//	        System.out.println("[INFO] 해당 회원이 존재하지 않습니다.");
-//	        return false;
-//	    }
-//	}
-//
+//			
+//		    mdto = memberService.memberSearchOne("Role_user", id);
+//		    if (mdto != null) {
+//		    	pdto = productService.reqProd(product_code); 
+//		        if (pdto != null) {
+//		        	
+//		        	
+//		            int currentStock = pdto.getAmount();
+//		            
+//		           
+//		            	if (currentStock >= odr_amount) {
+//		                int updatedStock = currentStock - odr_amount;
+//		                pdto.setAmount(updatedStock);
+//		                productService.updateProdAmount(product_code, pdto);
+//		                int pp = odr_amount * pdto.getProd_price();
+//		                
+//		                String oid = UUID.randomUUID().toString();
+//		                odto.setOrder_id(oid); // 주문 ID 설정
+//		                odto.setProduct_code(product_code);
+//		                oDao.insert(new OrderDto("1",mdto.getId(),pdto.getProduct_code(),pdto.getProduct_name(),mdto.getAdr_addr(),odr_amount,null,pp));
+//            	
+//		                System.out.println("[INFO] 주문완료");		                
+//		                return true;	            
+//		            }		                         
+//		            else 
+//		            {
+//		                System.out.println("[INFO] 주문 수량이 재고보다 많습니다.");
+//		                return false;
+//		            }
+//		    }
+//		       else 
+//		       {
+//		            System.out.println("[INFO] 해당 상품이 존재하지 않습니다.");
+//		            return false;
+//		       }
+//		  
+//		    }
+//		    	else
+//		    {
+//		        System.out.println("[INFO] 해당 회원이 존재하지 않습니다.");
+//		        return false;
+//		    }
+//}
 //	
-	
-	public boolean reqOrder(String sid, String id,int product_code, int odr_amount) throws Exception {
-		MemberDto mdto = new MemberDto();
-		ProdDto pdto = new ProdDto();
-		OrderDto odto = new OrderDto();
 		
-		String role = memberService.getRole(sid);
-		if (!role.equals("Role_Member")) {
-		      System.out.println("[WARN] 관리자만 로그인 할 수 있습니다.");
-		      return false;
+		// ---------------------------------------------------------------- 테스트 ------------------------------------------
+		
+		// -------------------------------------------------------------------------------------- 민영씨 코드
+		public boolean reqOrder2(String sid, String id, int product_code, int odr_amount) throws Exception {
+		    MemberDto mdto = new MemberDto();
+		    ProdDto pdto = new ProdDto();
+		    OrderDto odto = new OrderDto();
+
+		    String role = memberService.getRole(sid);
+		    if (!role.equals("Role_user")) {
+		        System.out.println("[WARN] 회원만 로그인 할 수 있습니다.");
+		        return false;
+		    }
+
+		    mdto = memberService.memberSearchOne(role, id);
+		    if (mdto != null) {
+		        pdto = productService.reqProd(product_code);
+		        if (pdto != null) {
+		            int currentStock = pdto.getAmount();
+		            if (currentStock >= odr_amount) {
+		                int updatedStock = currentStock - odr_amount;
+		                pdto.setAmount(updatedStock);
+		                // tbl_product 의 내용 수정(ok)
+		                
+		                // insert 유무에 관하여 T/F 설정
+		                boolean updateSuccess = productService.updateProdAmount(product_code, pdto); // 현재 상품재고량 수정
+
+		                // insert가 되면 상품 재고 업데이트를 실행
+		                if (updateSuccess) {
+		                    int pp = odr_amount * pdto.getProd_price();
+		                    String oid = UUID.randomUUID().toString();
+
+		                    odto.setOrder_id(oid); // 주문 ID 설정
+		                    odto.setProduct_code(product_code);
+
+		                    // tbl_order 의 내용 추가(x)
+		                    
+		                    // insert 여부를 확인하는 코드
+		                    int insertSuccess = oDao.insert(new OrderDto(oid, mdto.getId(), pdto.getProduct_code(),
+		                            pdto.getProduct_name(), mdto.getAdr_addr(), odr_amount, null, pp));
+
+		                    // insert 가 무사히 되었을시 true값을 리턴
+		                    if (insertSuccess==1) {
+		                        System.out.println("[INFO] 주문완료");
+		                        return true;
+		                        
+		                    } 
+		                    // insert가 되지 않았을시에 false값을 리턴
+		                    else {
+		                        System.out.println("[INFO] 주문 내역 추가 실패");
+		                        // 주문 내역 추가 실패 시 상품 재고량을 원래대로 복구
+		                        pdto.setAmount(currentStock);
+		                        productService.updateProdAmount(product_code, pdto);
+		                        return false;
+		                    }
+		                } 
+		                // insert가 되지 않았을시 상품재고량 업데이트하지 않고 false 값을 리턴
+		                else {
+		                    System.out.println("[INFO] 상품 재고량 업데이트 실패");
+		                    return false;
+		                }
+		            } else {
+		                System.out.println("[INFO] 주문 수량이 재고보다 많습니다.");
+		                return false;
+		            }
+		        } else {
+		            System.out.println("[INFO] 해당 상품이 존재하지 않습니다.");
+		            return false;
+		        }
+		    } else {
+		        System.out.println("[INFO] 해당 회원이 존재하지 않습니다.");
+		        return false;
+		    }
 		}
 		
-	    mdto = memberService.memberSearchOne("Role_user", id);
-	    if (mdto != null) {
-	    	pdto = productService.reqProd(product_code); 
-	        if (pdto != null) {
-	            int currentStock = pdto.getAmount();
-	            if (currentStock >= odr_amount) {
-	                int updatedStock = currentStock - odr_amount;
-	                pdto.setAmount(updatedStock);
-	                productService.updateProd(id, pdto);
-	                
-	                odto.setOrder_id(UUID.randomUUID().toString()); // 주문 ID 설정
-	                odto.setProduct_code(product_code);
-	                
-	                oDao.insert(odto);
-	                System.out.println("[INFO] 주문완료");
-	                return true;
-	            } else {
-	                System.out.println("[INFO] 주문 수량이 재고보다 많습니다.");
-	                return false;
-	            }
-	        } else {
-	            System.out.println("[INFO] 해당 상품이 존재하지 않습니다.");
-	            return false;
-	        }
-	    } else {
-	        System.out.println("[INFO] 해당 회원이 존재하지 않습니다.");
-	        return false;
-	    }
-	}
-
-	
-	
-	//==================================피드백
-	
-	
-	// 모드 주문확인
-	public List<OrderDto> getAllOrder()
-	{
-		System.out.println("OrderService's getAllOrder()");
-		return oDao.select();
 		
-	}
+		//-----------------------------------------------------------------------------------------
+		
+		
+		// ---------------------------------------------------------------- 테스트 ------------------------------------------		
+		    
+		
 	
 	// 건별 주문 확인
 	public OrderDto getOrder(String order_id)
